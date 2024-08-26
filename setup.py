a49acda7995c3dc74cacb3e9c68e3eec8366cc65
Darpan Desai <39281473+darpandesai28@users.noreply.github.com>
2024-08-26 17:06:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,3 +8,14 @@ setuptools.setup(
     py_modules = ['cde_functions'],     
     python_requires = '>=3.6'
 )
+
+
+from setuptools import setup
+
+setup(name='cde_functions',
+        version='1.0',
+        description='Custom functions created for use within databricks.',        
+        author=['Corey Lesko','Darpan Desai'],
+        license='',
+        packages=['cde_functions'],
+        python_requires='>=3.6')
